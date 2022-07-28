d36568fbc593e431faaf549a2764e4583457d2e5
Januar Soepangat <januar.soepangat@gmail.com>
2022-07-28 03:28:52 -0500

    update

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
 "use strict";
 
-/** Express app for showdown **/
+/** Express app for jobly. */
 
 const express = require("express");
 const cors = require("cors");
 
 const { NotFoundError } = require("./expressError");
 
+const schoolsRoutes = require("./routes/schools");
+const usersRoutes = require("./routes/users");
+
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+}
+
+app.use("/schools", schoolsRoutes);
+app.use("/users", usersRoutes);
+
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+  return next(new NotFoundError());
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+  if (process.env.NODE_ENV !== "test") console.error(err.stack);
+  const status = err.status || 500;
+  const message = err.message;
+
+  return res.status(status).json({
+    error: { message, status },
+  });
+});
+
+module.exports = app;
